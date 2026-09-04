5080dd9667bcff9948bd2136b8cebf8774312c31
roflsunriz <roflsunriz@yahoo.co.jp>
2026-09-05 01:22:38 +0900

    chore(release): 1.4.1とAndroid 0.2.2を準備

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.nytweetdeck.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.2.1"
+        versionCode = 7
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
