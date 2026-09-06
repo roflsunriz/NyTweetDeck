11ffc2c38545aa0040155378cc641df19fd90979
roflsunriz <roflsunriz@yahoo.co.jp>
2026-09-06 14:44:51 +0900

    chore(release): デスクトップ1.6.0とAndroid0.4.0を準備

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.nytweetdeck.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.3.0"
+        versionCode = 11
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
